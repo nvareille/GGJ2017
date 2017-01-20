cc94c66f38f146a55f84e180e571170242303216
Nicolas VAREILLE <nicolas.vareille@epitech.eu>
2017-01-21 00:59:12 +0100

    Add BAsic fishing pole

---
+++ GGJ2017/Assets/Scripts/GamePlay/HookMover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.GamePlay;
+using UnityEngine;
+
+public class HookMover : MonoBehaviour
+{
+    public float SpeedModifier = 1f;
+
+    public TetherMovement TetherMovement;
+    public Animator Animator;
+    public Animation A;
+
+    public void Update()
+    {
+        float tether = Input.GetAxis("Horizontal");
+
+        TetherMovement.Transform.position += new Vector3(tether, 0, 0) * SpeedModifier * Time.deltaTime;
+        TetherMovement.AddTether(-tether);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Animator.SetBool("Casting", true);
+        }
+        else
+        {
+            Animator.SetBool("Casting", false);
+        }
+    }
+}
---
+++ GGJ2017/Assets/Scripts/GamePlay/TetherMovement.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.GamePlay
+{
+    public class TetherMovement : MonoBehaviour
+    {
+        public RectTransform Transform;
+
+        public float TetherModifier = 1f;
+        public float InertModifier = 1f;
+        public float LimitedAngle = 45f;
+        public float Tether;
+
+        public float Pendulum;
+        public bool PendulumWay;
+
+        private bool Moved;
+
+        public void Start()
+        {
+            PendulumWay = false;
+        }
+
+        public void AddTether(float strenght)
+        {
+            Moved = true;
+            Tether += strenght * 2;
+            Pendulum += Math.Abs(strenght);
+            if (Pendulum > 1)
+                Pendulum = 1;
+            if (Tether > LimitedAngle)
+                Tether = LimitedAngle;
+            if (Tether < -LimitedAngle)
+                Tether = -LimitedAngle;
+        }
+
+        public void AffectPendulum()
+        {
+            //Pendulum += Time.deltaTime * 100;
+        }
+
+        public void Update()
+        {
+            AffectPendulum();
+
+            float f = Tether * Pendulum;
+
+            Transform.rotation = Quaternion.Euler(new Vector3(0, 0, (f > 1 && f < -1 ? 0 : f)));
+            ReduceTether();
+        }
+
+        public void ReduceTether()
+        {
+            Tether -= (Tether > 0 ? Time.deltaTime : -Time.deltaTime) * TetherModifier;
+            if (Tether <= 0.1 && Tether >= -0.1)
+                Tether = 0;
+        }
+    }
+}
