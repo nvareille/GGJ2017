98760bf29b9d97599b0c824c47675cf358b2e086
miestl <loic.miest@epitech.eu>
2017-01-21 13:55:16 +0100

    suite anim and aliment spawner

---
+++ GGJ2017/Assets/_script/AnimatorManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorManager : MonoBehaviour {
+
+	public bool play;
+	// Use this for initialization
+	void Start () {
+		//_play = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//_play = this.GetComponent<Animator> ().GetBool ("stopAliment");
+		//Debug.Log (_play);
+		if (!play)
+			Destroy (this.gameObject);
+		//if (this.GetComponent<Animator> ().GetCurrentAnimatorStateInfo (0).IsName ("animAliment"))
+		//	Destroy (this);
+	}
+	void Lol()
+	{
+		/*if (!_play) {
+			Debug.Log ("passe");
+			Destroy (this.gameObject);
+		}*/
+	}
+}
---
+++ GGJ2017/Assets/_script/alimentSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alimentSpawner : MonoBehaviour {
+
+	public GameObject aliment;
+	private float Timer;
+
+	// Use this for initialization
+	void Start () {
+		Timer = Time.time + 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (Timer < Time.time) 
+		{
+			Instantiate (aliment, transform.position, transform.rotation);
+			Timer = Time.time + 3;
+		}
+	}
+
+	void spawn ()
+	{
+
+	}
+}
