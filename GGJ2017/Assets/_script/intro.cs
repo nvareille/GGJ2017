39a30e81fcce4c2bfd95710bc08a51b98df962ef
miestl <loic.miest@epitech.eu>
2017-01-21 09:22:44 +0100

    start animation
    animation intro : cat, fishing pool, waves, aliment

---
+++ GGJ2017/Assets/_script/intro.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class intro : MonoBehaviour {
+
+	public GameObject[] elem;
+
+	public GameObject Cat; // apparait à gauche  // 2
+	public GameObject FishingPool; // apparait à gauche // 2
+	public GameObject Dock; // apparait à gauche // 1
+	public GameObject Waves; // apparait à droite et en bas ~ 1, 2, 3
+	public GameObject Marmite; // apparait à gauche // 3 // pas sur
+
+	public float[] posXelem;
+	public float[] posYelem;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
