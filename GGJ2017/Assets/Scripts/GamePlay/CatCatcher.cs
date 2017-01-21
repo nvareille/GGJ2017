1e6aaf59d253a56906b765d9f37bae536a78b6c3
miestl <loic.miest@epitech.eu>
2017-01-21 17:18:46 +0100

    containerOfElem

--- GGJ2017/Assets/Scripts/GamePlay/CatCatcher.cs
+++ GGJ2017/Assets/Scripts/GamePlay/CatCatcher.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using Assets.Scripts.ScriptableObjects;
 
 namespace Assets.Scripts.GamePlay
 {
     public class CatCatcher : MonoBehaviour
     {
+		public bool asCaught = false;
+		public AlimentKind getAliment;
+		public addElement _add;
         void OnTriggerEnter2D(Collider2D c)
         {
             Debug.Log(c.name);
@@ -15,6 +19,9 @@ namespace Assets.Scripts.GamePlay
             if (c.gameObject.name.Contains("aliment"))
             {
                 c.gameObject.GetComponent<Aliment>().Parent.GetComponent<Animator>().SetBool("Caught", true);
+				asCaught = true;
+				getAliment = c.gameObject.GetComponent<Aliment>().AlimentKind;
+				_add.addElemToArray ();
             }
         }
     }
---
+++ GGJ2017/Assets/_script/addElement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using  Assets.Scripts.GamePlay;
+using Assets.Scripts.ScriptableObjects;
+
+public class addElement : MonoBehaviour {
+
+
+	public AlimentKind[] arrayElem;
+	public CatCatcher tocatch;
+	int i = 0;
+
+	// 
+	// Use this for initialization
+	void Start () {
+		arrayElem = new AlimentKind[5];	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+	}
+
+	void defineTheSizeOfArray()
+	{
+
+
+	}
+
+	public void addElemToArray()
+	{
+			if (tocatch.asCaught)
+				arrayElem [i] = tocatch.getAliment;
+			i++;
+	}
+}
