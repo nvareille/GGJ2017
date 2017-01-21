c4a267c64e3c0f9e7f779758b2ef93c90892c71c
tiphaine.perra@epitech.eu <tiphaine.perra@epitech.eu>
2017-01-21 12:44:26 +0100

    [UIMenu] Hierarchie Menu + Script

---
+++ GGJ2017/Assets/Scripts/Menu/Play.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlayButton(){
+		Debug.Log ("I'm the play button !!");
+	}
+}
