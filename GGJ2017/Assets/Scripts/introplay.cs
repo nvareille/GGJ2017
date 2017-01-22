6b531590c1d69671ebcc1519d9515b87d3d1bd3e
Severine Febvet <severinefebvet@gmail.com>
2017-01-22 15:43:27 +0100

    intro

---
+++ GGJ2017/Assets/Scripts/introplay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class introplay : MonoBehaviour {
+
+	MovieTexture tex;
+
+	// Use this for initialization
+	void Start () {
+		tex =  (MovieTexture)gameObject.GetComponent<MeshRenderer>().material.mainTexture;
+		tex.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!tex.isPlaying)
+			StartCoroutine("WaitAndStart");
+	}
+
+	IEnumerator WaitAndStart()
+	{
+		yield return new WaitForSeconds(1.5f);
+		SceneManager.LoadScene("UIMenu");
+	}
+}
