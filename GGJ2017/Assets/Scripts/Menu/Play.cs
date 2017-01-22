45cef7710429ad5593d055c9a29cc17a3b597688
Nicolas VAREILLE <nicolas.vareille@epitech.eu>
2017-01-22 15:21:14 +0100

    Add game selection working

--- GGJ2017/Assets/Scripts/Menu/Play.cs
+++ GGJ2017/Assets/Scripts/Menu/Play.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Assets.Scripts.ScriptableObjects;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Play : MonoBehaviour {
-
-	// Use this for initialization
+public class Play : MonoBehaviour
+{
+    // Use this for initialization
 	void Start () {
 		
 	}
--- GGJ2017/Assets/Scripts/UI/RecipeSelection.cs
+++ GGJ2017/Assets/Scripts/UI/RecipeSelection.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Assets.Scripts.GamePlay;
+using Assets.Scripts.ScriptableObjects;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,6 +11,16 @@ namespace Assets.Scripts.UI
 {
     public class RecipeSelection : MonoBehaviour
     {
+        public Recipe[] Recipes;
+        public RecipeManager Manager;
+
+        public void SelectRecipe(int r)
+        {
+            Manager.Recipe = Recipes[r];
+            DontDestroyOnLoad(Manager.gameObject);
+            SceneManager.LoadScene(1);
+        }
+
         public void Return()
         {
             SceneManager.LoadScene(0);
